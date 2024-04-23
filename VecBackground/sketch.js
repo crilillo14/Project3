5386b1ec1f7da7306ba142a287f046620966c990
crilillo14 <93829520+crilillo14@users.noreply.github.com>
2024-04-22 23:18:20 -0400

    Migrated from old repo
    Couldnt figure out submodule removal in git

---
+++ VecBackground/sketch.js
@@ -0,0 +1,205 @@
+
+
+let Field;
+let vectorScaling = 0.06;
+let gapBetweenRows;
+let gapBetweenCols;
+let counter = 0;
+let weights = [];
+let maxspeed = 1;
+let resetchance = 0.005;
+
+let Functions, functionPicker;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  Field = generateField(30, 30);
+  angleMode(degrees)
+
+
+
+  Functions = [
+    function(p) {
+      
+      let x = p.x * cos(p.y);
+      let y = sqrt(abs(p.x));
+      return createVector(x , y);
+    },
+    function(p) {
+      let x = -p.y + 0.5*p.x;
+      let y = p.x;
+      return createVector(x , y);
+    }, 
+    function(p) {
+      let x = max(p.mag() + cos(p.y) , p.y)
+      let y = p.y / sin(p.y)
+      return createVector(x , y);
+    },
+    function(p) {
+      return createVector(p.x, p.y)
+    },
+    function(p) {
+      let x = -10000 * p.y / (p.x*p.x + p.y*p.y)
+      let y = 10000 * p.x / (p.x*p.x + p.y*p.y)
+      
+      return createVector(x , y)
+    }, 
+    function(p) {
+      let x = 1000 * p.x / (p.x*p.x + p.y*p.y)
+      let y = 10000 * p.y / (p.x*p.x + p.y*p.y)
+      
+      return createVector(x , y)
+    },
+    function(p) {
+      let x = 2000 * p.x / (p.x*p.x + p.y*p.y)
+      let y = 2000 * p.y / (p.x*p.x + p.y*p.y)
+      
+      return createVector(x , y)
+    }, 
+    function(p) {
+      let x = p.x * p.y /(p.x + p.y)
+      let y =( p.x*p.x + p.y*p.y) / (p.x + p.y);
+      return createVector(x , y)
+    }, 
+    function(p) {
+      let x = p.x;
+      let y = -0.5*p.y + 2500*sin(0.01*p.x)
+      return createVector(x , y)
+    } ,
+    function(p) {
+      let x = 3 * Math.sign(p.y)
+      let y = 0.01*p.y 
+      return createVector(x , y)
+    }, 
+    function(p) {
+      let x = 0.1 * p.x
+      let y = 200*sin(0.04*(p.mag() + 0.1 * p.y))
+      return createVector(x  , y)
+    }
+
+
+  ]
+
+  functionPicker = floor(random(0 , Functions.length))
+
+
+  
+  
+}
+
+function draw() {
+  colorMode(RGB)
+  background(0,0, 20,5);
+
+  translate(width / 2, height / 2);
+  scale(1, -1);
+
+  noStroke()
+
+  //circle(0, 0, 3);
+
+  
+  //visualizeVelocityField(Field)
+
+  for (i = 0; i < Field.length; i++) {
+  for (j = 0; j < Field[i].length; j++) {
+    let positionVector = Field[i][j][Field[i][j].length - 1]; // get the last position vector
+    let newposition = positionVector.copy();
+    let velocityVector = CalculateVelocity(newposition);
+    newposition = newposition.add(velocityVector); // add velocity to position
+    Field[i][j].push(newposition); // push the new position
+
+
+
+    for(k = 0; k < Field[i][j].length; k++){
+      //let alpha = 255 / (k + 1); // Add 1 to avoid division by zero
+      
+          
+      circle(Field[i][j][k].x , Field[i][j][k].y , 1); // show position
+    }
+    
+
+    if(Field[i][j].length > 20) {
+      Field[i][j].splice(0, 1)
+    }
+
+    if(velocityVector.mag() < .1) { // if the particle is moving too slow, take away from the front
+      Field[i][j].splice(Field[i][j].length - 1, 1)
+    }
+
+
+    if(random(0, 1) < resetchance) {
+      Field[i][j] = getInitialFieldPosition(i , j);
+    }
+
+
+  }
+}
+
+} // ---------------------------------------------------- end of draw()
+
+
+
+
+
+function CalculateVelocity(p) {
+  
+  let vector = Functions[functionPicker](p);
+  vector.mult(vectorScaling)
+  vector.setMag(min(vector.mag(), maxspeed))
+
+  // Calculate the color based on the vector's direction
+  let hue = (vector.heading() + PI) / (2 * PI) * 255;
+  let saturation = 255;
+  let brightness =  255;
+  
+  // Set the color mode to HSB and the stroke color to the calculated color
+  colorMode(HSB);
+  //stroke(hue, saturation, brightness, 255);
+  fill(hue, saturation, brightness, 255); // Set the fill color to the same color as the stroke
+
+  return vector;
+
+} //---------------------------------------------- end of vectorFunction()
+
+
+
+
+
+
+
+
+
+
+
+// Field Generation and getting initial positions
+
+
+function generateField(nrows, ncols) {
+  let field = [];
+  
+  gapBetweenRows = width / nrows;
+  gapBetweenCols = height / ncols;
+  
+  for (let i = 0; i <= nrows; i++) {
+    let ColumnVector = [];
+    
+    for (let j = 0; j <= ncols; j++) {
+      let x = (i * gapBetweenRows) - width / 2;
+      let y = -1*((j * gapBetweenCols) - height / 2 );
+      
+      ColumnVector.push([createVector(x, y)]);
+    }
+    
+    field.push(ColumnVector);
+  }
+
+  return field;
+} // --------------------------------------------- end of generateField()
+
+
+function getInitialFieldPosition(i, j) {
+  let x = (i * gapBetweenRows) - width / 2;
+  let y = -1*((j * gapBetweenCols) - height / 2);
+  return [createVector(x , y)]
+}
---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+var myButton = document.querySelector('#button');
+
+var home = document.querySelector(home)
+// Add a click event listener to the button
+myButton.addEventListener('click', function() {
+    // Use fullPage.js's moveTo method to scroll to the first section
+    fullpage_api.moveTo(1);
+});
+
+new fullpage('#fullpage', {
+    // options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    anchors: ['home' , 'about' , 'projects' , 'contact'],
+
+    afterLoad: function(origin, destination, direction){
+        var loadedSection = this;
+
+        //using index
+        if(destination.index !== 0){
+            // show your button when not in the first section
+            document.querySelector('#button').style.position = 'fixed';
+            document.querySelector('#button').style.display = 'flex';
+            document.querySelector('#button').style.opacity = '1';
+        } else {
+            // hide your button when in the first section
+            document.querySelector('#button').style.display = 'none';
+            document.querySelector('#button').style.opacity = '0';
+        }
+    },
+
+    onLeave: function(origin, destination, direction){
+        var leavingSection = this;
+
+        //using index
+        if(origin.index === 0 && direction =='down'){
+            // stick the button to the second section when scrolling down from the first section
+            //document.querySelector('#button').style.position = 'absolute';
+            document.querySelector('#button').style.opacity = '1';
+
+        } else if(origin.index === 1 && direction =='up'){
+            // hide the button when scrolling up to the first section
+            //document.querySelector('#button').style.display = 'none';
+            document.querySelector('#button').style.opacity = '0';
+        }
+    }
+
+});
+
+
+
+
+
